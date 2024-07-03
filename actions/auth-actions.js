78a2cc67a3810a10a6346500ae7ef5d6bb29828b
Vrunda <vrunda1002@gmail.com>
2024-07-03 18:53:39 +0530

    Using Lucia to implement authentication

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,5 +1,6 @@
 'use server';
 
+import {createAuthSession} from '@/lib/auth';
 import {hashUserPassword} from '@/lib/hash';
 import {createUser} from '@/lib/user';
 import {redirect} from 'next/navigation';
@@ -26,7 +27,9 @@ export async function signup(_, formData) {
 
   const hashedPassword = hashUserPassword(password);
   try {
-    createUser(email, hashedPassword);
+    const userId = createUser(email, hashedPassword);
+    await createAuthSession(userId);
+    redirect('/training');
   } catch (error) {
     if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
       return {
@@ -37,6 +40,4 @@ export async function signup(_, formData) {
     }
     throw error;
   }
-
-  redirect('/training');
 }
--- app/training/page.js
+++ app/training/page.js
@@ -1,12 +1,19 @@
+import { verifyAuth } from '@/lib/auth';
 import { getTrainings } from '@/lib/training';
+import { redirect } from 'next/navigation';
 
 export default async function TrainingPage() {
+  const result = await verifyAuth();
+
+  if (!result.user) {
+    return redirect('/');
+  }
   const trainingSessions = getTrainings();
 
   return (
     <main>
       <h1>Find your favorite activity</h1>
-      <ul id="training-sessions">
+      <ul id='training-sessions'>
         {trainingSessions.map((training) => (
           <li key={training.id}>
             <img src={`/trainings/${training.image}`} alt={training.title} />
