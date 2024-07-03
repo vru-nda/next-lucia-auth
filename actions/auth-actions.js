f4a8e85ca2739f94456ddf2480301bb2e8b4749e
Vrunda <vrunda1002@gmail.com>
2024-07-03 18:52:32 +0530

    Hash password and store user

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,5 +1,9 @@
 'use server';
 
+import {hashUserPassword} from '@/lib/hash';
+import {createUser} from '@/lib/user';
+import {redirect} from 'next/navigation';
+
 export async function signup(_, formData) {
   const email = formData.get('email');
   const password = formData.get('password');
@@ -19,4 +23,20 @@ export async function signup(_, formData) {
       errors,
     };
   }
+
+  const hashedPassword = hashUserPassword(password);
+  try {
+    createUser(email, hashedPassword);
+  } catch (error) {
+    if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+      return {
+        errors: {
+          email: 'Email already exist',
+        },
+      };
+    }
+    throw error;
+  }
+
+  redirect('/training');
 }
